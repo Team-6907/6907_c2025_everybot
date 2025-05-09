2ee22593bc97320e1a223719fa81a9f2b1e72c8a
FLYMOUSE-WANG <gracefulcrypter@126.com>
2025-05-09 21:40:37 +0800

    Updated part of arm subsystem

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,17 +1,100 @@
 // Copyright (c) 2025 FRC 6907, The G.O.A.T
 package frc.robot.subsystems.arm;
 
+import static edu.wpi.first.units.Units.*;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.measure.Angle;
 import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj.DriverStation;
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
-public class Arm extends SubsystemBase {
+import lombok.Getter;
+
+public class Arm extends SubsystemBase{
   private final ArmIO io;
   private final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
 
+  private Angle setpoint = Degrees.of(0);
+  private Angle currentGoal = Degrees.of(0);
+
+  private enum ArmState {
+    TOP(Degrees.of(0)),
+    BOTTOM(Degrees.of(0));
+
+    private Angle setpoint = Degrees.of(0);
+
+    ArmState(Angle setpoint) {
+      this.setpoint = setpoint;
+    }
+
+    public Angle getSetpoint() {
+      return setpoint;
+    }
+  }
+
+  public static final Angle AT_SETPOINT_TOLERANCE = Degrees.of(2.0);
+
+  public static final Angle ARM_MIN_ANGLE = Degrees.of(-360.0);
+  public static final Angle ARM_MAX_ANGLE = Degrees.of(360.0);
+  public static final Angle ARM_STARTING_ANGLE = Degrees.of(0.0);
+
+  @Getter
+  @AutoLogOutput(key = "Arm/AtGoal")
+  private boolean atGoal = false;
+
+  @AutoLogOutput @Getter private boolean homed = false;
+
+  private Debouncer goalDebouncer = new Debouncer(0.1, Debouncer.DebounceType.kRising);
+
   public Arm(ArmIO io) {
     this.io = io;
+  }
+
+
+  public void goToPosition(ArmState armState) {
+    setpoint = armState.getSetpoint();
+    if (setpoint instanceof Angle angleSetpoint) {
+      Angle clampedSetpoint = angleSetpoint;
+      if (angleSetpoint.lt(ARM_MIN_ANGLE)) {
+        clampedSetpoint = ARM_MIN_ANGLE;
+      } else if (angleSetpoint.gt(ARM_MAX_ANGLE)) {
+        clampedSetpoint = ARM_MAX_ANGLE;
+      } 
+
+      this.setpoint = clampedSetpoint;
+      this.currentGoal = clampedSetpoint;
+      this.atGoal = false;
+      goalDebouncer.calculate(false);
+      Logger.recordOutput("Arm/CommandedSetpoint", clampedSetpoint.in(Degrees));
+    } else {
+      DriverStation.reportError(
+        "["
+              + "Arm"
+              + "] Invalid setpoint type. Expected Angle, got "
+              + setpoint.getClass().getSimpleName(),
+          false);
+    }
+  }
+
+  public Measure<AngleUnit> getCurrentPosition() {
+    return inputs.position;
+  }
+
+  public void stop() {
+    io.stop();
+    // Update setpoint to reflect current position when stopped externally
+    this.setpoint = inputs.position;
+    this.currentGoal = inputs.position;
+    this.atGoal = false; // Reset atGoal when stopped
+    goalDebouncer.calculate(false); // Reset debouncer state
+    Logger.recordOutput("Arm/Stopped", true);
   }
 
   @Override
@@ -28,5 +111,10 @@ public class Arm extends SubsystemBase {
     return runEnd(
         () -> io.setVoltage((forward.getAsDouble() - reverse.getAsDouble()) * 12.0),
         () -> io.setVoltage(0.0));
+  }
+
+    public Command setSetpointCommand(ArmState targetSetpoint) {
+    return Commands.runOnce(() -> goToPosition(targetSetpoint))
+        .withName("ArmInternalSet_" + String.format("%.1f", targetSetpoint));
   }
 }
--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -1,20 +1,45 @@
 // Copyright (c) 2025 FRC 6907, The G.O.A.T
 package frc.robot.subsystems.arm;
 
+import static edu.wpi.first.units.Units.*;
+
 import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.units.measure.MutAngle;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import edu.wpi.first.units.measure.MutCurrent;
+import edu.wpi.first.units.measure.MutVoltage;
 
 public interface ArmIO {
   @AutoLog
-  public static class ArmIOInputs {
-    public double positionRad = 0.0;
-    public double velocityRadPerSec = 0.0;
-    public double appliedVolts = 0.0;
-    public double currentAmps = 0.0;
+  public static class ArmIOInputs {    
+    public MutAngle position = Degrees.mutable(0);
+    public MutAngularVelocity velocity = DegreesPerSecond.mutable(0);
+
+    public MutVoltage appliedVolts = Volts.mutable(0);
+    public MutCurrent currentAmps = Amps.mutable(0);
+
+    public MutAngle setpointPosition = Degrees.mutable(0);
+    public MutAngularVelocity setpointVelocity = DegreesPerSecond.mutable(0);
   }
 
   /** Update the set of loggable inputs. */
   public default void updateInputs(ArmIOInputs inputs) {}
 
+  public default void runSetpoint(Angle position) {}
+
+  public default void runVolts(Voltage voltage) {}
+
+  public default void resetPosition(Angle position) {}
+
+  public default void setPID(double p, double i, double d) {}
+
+  public default void setFF(double kS, double kG, double kV, double kA) {}
+
   /** Run open loop at the specified voltage. */
   public default void setVoltage(double volts) {}
+
+  public default void stop() {}
 }
